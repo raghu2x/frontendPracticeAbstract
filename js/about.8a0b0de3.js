d0d5c7e37cc7b07584b57773da56cc766ded7c69
raghvendra <raghvendra4077@gmail.com>
2022-12-14 00:53:01 +0530

    Updates

---
+++ js/about.8a0b0de3.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[443],{507:function(n,t,u){u.r(t),u.d(t,{default:function(){return i}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],e=u(1),l={},r=(0,e.Z)(l,s,a,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.8a0b0de3.js.map
